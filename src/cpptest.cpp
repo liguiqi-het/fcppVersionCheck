ccfc27b2fd098085daffa4705fa821f479f0d24e
liguiqi <guiqi.li@szhittech.com>
2026-01-14 17:28:22 +0800

    fffix: release buiid on test × V1.11

--- src/cpptest.cpp
+++ src/cpptest.cpp
@@ -113,3 +113,4 @@ void test_cpp_zlib() {
 
 // release buiid on------ fix: release buiid on test √ V1.11
 // release buiid on------ ffix: release buiid on test × V1.11
+// release buiid on------ fffix: release buiid on test × V1.11
