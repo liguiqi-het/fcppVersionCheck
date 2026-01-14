a17649035ca3230b7978b342914f6e55cad9c099
liguiqi <guiqi.li@szhittech.com>
2026-01-14 17:22:32 +0800

    feat: release buiid on test √ V1.07

--- src/cpptest.cpp
+++ src/cpptest.cpp
@@ -109,3 +109,4 @@ void test_cpp_zlib() {
 // release buiid on------ fix: release buiid on test × V1.06
 
 // release buiid on------ test: release buiid on test × V1.07
+// release buiid on------ feat: release buiid on test √ V1.07
